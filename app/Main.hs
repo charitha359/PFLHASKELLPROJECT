d9acb2e182be88912183b0a9639a327f7e3c6a04
charitha359 <charithamettupalle@gmail.com>
2026-03-28 18:19:09 +0000

    final fix: server always runs

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
 
+import Control.Exception (catch)
 import Web.Scotty
 import qualified Data.ByteString.Lazy as BL
 import Data.Csv
@@ -45,148 +46,131 @@ main = do
 
   putStrLn "Loading Waste Dataset..."
 
-  csvData <- BL.readFile "data/waste.csv"
+  -- SAFE FILE READ (prevents crash)
+  csvData <- BL.readFile "data/waste.csv" `catch` (\_ -> return "")
 
-  case decode NoHeader csvData of
-    Left err -> print err
+  -- SAFE PARSE (server will still run even if CSV fails)
+  let wasteList =
+        case decode NoHeader csvData of
+          Left _ -> []
+          Right rows -> V.toList (rows :: V.Vector Waste)
 
-    Right rows -> do
+  putStrLn ("Records Loaded: " ++ show (length wasteList))
+  putStrLn "Starting server on port 10000..."
 
-      let wasteList = V.toList (rows :: V.Vector Waste)
-
-      putStrLn ("Records Loaded: " ++ show (length wasteList))
-      putStrLn "Server running at http://localhost:3000"
-
-      scotty 10000 $ do
+  -- IMPORTANT: Render port
+  scotty 10000 $ do
 
 -------------------------------------------------
 -- STATIC FILE SERVER
 -------------------------------------------------
 
-        middleware $ staticPolicy (noDots >-> addBase ".")
+    middleware $ staticPolicy (noDots >-> addBase ".")
 
 -------------------------------------------------
 -- ROOT DASHBOARD
 -------------------------------------------------
 
-        get "/" $ do
-          setHeader "Content-Type" "text/html"
-          file "index.html"
+    get "/" $ do
+      setHeader "Content-Type" "text/html"
+      file "index.html"
 
 -------------------------------------------------
 -- ALL DATA
 -------------------------------------------------
 
-        get "/data" $
-          json wasteList
+    get "/data" $
+      json wasteList
 
 -------------------------------------------------
 -- RECORD COUNT
 -------------------------------------------------
 
-        get "/count" $
-          json (length wasteList)
+    get "/count" $
+      json (length wasteList)
 
 -------------------------------------------------
 -- AVAILABLE YEARS
 -------------------------------------------------
 
-        get "/years" $ do
-          let yearsList = sort . nub $ map year wasteList
-          json yearsList
+    get "/years" $ do
+      let yearsList = sort . nub $ map year wasteList
+      json yearsList
 
 -------------------------------------------------
 -- GLOBAL WASTE TREND
 -------------------------------------------------
 
-        get "/yearly-total" $ do
-
-          let yearsList = sort . nub $ map year wasteList
-
-          let totals =
-                map (\y ->
-                    (y,
-                     sum [waste w | w <- wasteList, year w == y])
-                ) yearsList
-
-          json totals
+    get "/yearly-total" $ do
+      let yearsList = sort . nub $ map year wasteList
+      let totals =
+            map (\y ->
+                (y,
+                 sum [waste w | w <- wasteList, year w == y])
+            ) yearsList
+      json totals
 
 -------------------------------------------------
 -- TOP COUNTRIES
 -------------------------------------------------
 
-        get "/top-countries" $ do
-
-          let countries = nub $ map country wasteList
-
-          let totals =
-                map (\c ->
-                    (c,
-                     sum [waste w | w <- wasteList, country w == c])
-                ) countries
-
-          json (take 10 $ sortOn (Down . snd) totals)
+    get "/top-countries" $ do
+      let countries = nub $ map country wasteList
+      let totals =
+            map (\c ->
+                (c,
+                 sum [waste w | w <- wasteList, country w == c])
+            ) countries
+      json (take 10 $ sortOn (Down . snd) totals)
 
 -------------------------------------------------
 -- LEAST COUNTRIES
 -------------------------------------------------
 
-        get "/least-countries" $ do
-
-          let countries = nub $ map country wasteList
-
-          let totals =
-                map (\c ->
-                    (c,
-                     sum [waste w | w <- wasteList, country w == c])
-                ) countries
-
-          json (take 10 $ sortOn snd totals)
+    get "/least-countries" $ do
+      let countries = nub $ map country wasteList
+      let totals =
+            map (\c ->
+                (c,
+                 sum [waste w | w <- wasteList, country w == c])
+            ) countries
+      json (take 10 $ sortOn snd totals)
 
 -------------------------------------------------
 -- COUNTRY TREND
 -------------------------------------------------
 
-        get "/country-trend" $ do
-
-          let countries = nub $ map country wasteList
-
-          let trends =
-                map (\c ->
-                    (c,
-                     sortOn fst
-                       [ (year w, waste w)
-                       | w <- wasteList
-                       , country w == c ])
-                ) countries
-
-          json trends
+    get "/country-trend" $ do
+      let countries = nub $ map country wasteList
+      let trends =
+            map (\c ->
+                (c,
+                 sortOn fst
+                   [ (year w, waste w)
+                   | w <- wasteList
+                   , country w == c ])
+            ) countries
+      json trends
 
 -------------------------------------------------
 -- GLOBAL AVERAGE
 -------------------------------------------------
 
-        get "/global-average" $ do
-
-          let totalWaste = sum (map waste wasteList)
-
-          let avgWaste =
-                totalWaste / fromIntegral (length wasteList)
-
-          json avgWaste
+    get "/global-average" $ do
+      let totalWaste = sum (map waste wasteList)
+      let avgWaste =
+            totalWaste / fromIntegral (length wasteList)
+      json avgWaste
 
 -------------------------------------------------
 -- MAP DATA
 -------------------------------------------------
 
-        get "/map-data" $ do
-
-          let countries = nub $ map country wasteList
-
-          let totals =
-                map (\c ->
-                    (c,
-                     sum [waste w | w <- wasteList, country w == c])
-                ) countries
-
-          json totals
+    get "/map-data" $ do
+      let countries = nub $ map country wasteList
+      let totals =
+            map (\c ->
+                (c,
+                 sum [waste w | w <- wasteList, country w == c])
+            ) countries
+      json totals
